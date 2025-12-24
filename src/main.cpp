e056b7e392b6010c917105bfa467519fce06cec7
marobeck <maysgaming00@gmail.com>
2025-12-24 00:20:31 -0800

    Read from database

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
-// DEBUG
-#include <iostream>
+#include "log.h"
 
 // --- GUI ---
 #include "mainwindow.h"
 // --- Data handling ---
 #include "timeblock.h"
+#include "database.h"
 
 #include <QApplication> // UI app
 
 int main(int argc, char *argv[])
 {
+    const char *TAG = "main";
+
     QApplication app(argc, argv);
 
     MainWindow window;
@@ -20,38 +22,14 @@ int main(int argc, char *argv[])
 
     std::vector<Timeblock> Blocks;
 
-    // Pet a dog every day at 13:00 for ~20 minutes
+    // Initialize database
+    Database db;
+
+    // Load timeblocks from database
+    db.load_timeblocks(Blocks);
+    for (auto &tb : Blocks)
     {
-        Timeblock block1("Dog time", "Go meet a dog", 0x7F, (time_t)1000, (time_t)46800);
-
-        Task task1("Pet dog", "Pet a dog on the head", Priority::MEDIUM, 1765829922);
-        Task task2("See dog", "Witness a creature", Priority::VERY_HIGH);
-
-        block1.append(task1);
-        block1.append(task2);
-
-        Blocks.push_back(block1);
-    }
-    {
-        Timeblock block1("Sleep time", "Please sleep", 0x7F, (time_t)28800, (time_t)82800);
-
-        Blocks.push_back(block1);
-    }
-    {
-        Timeblock block1("Work time", "Get paid", 0x7F, (time_t)28800, (time_t)82800);
-
-        Task task1("Eat evidence", "", Priority::VERY_HIGH, 1764829922);
-        block1.append(task1);
-
-        Blocks.push_back(block1);
-    }
-    {
-        Timeblock block1("Soup time", "SOUP", 0x7D, (time_t)28800, (time_t)82800);
-
-        Task task1("Eat Soup", "", Priority::VERY_LOW, 1764929922);
-        block1.append(task1);
-
-        Blocks.push_back(block1);
+        db.load_tasks(&tb);
     }
 
     window.todoListView->updateTasklists(Blocks);
