7fa661baa55c7da5914cb69d1b39127ac2b7a045
marobeck <maysgaming00@gmail.com>
2026-01-03 17:22:14 -0800

    Colored log entries

--- src/log.h
+++ src/log.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 /* =========================
  *  Return Codes
@@ -58,22 +60,46 @@ static void log_print(log_level_t level, const char *tag, const char *fmt, ...) 
         default:              level_str = "?"; break;
     }
 
-    time_t t = time(NULL);
-    struct tm *lt = localtime(&t);
-    char timestr[9];
-    if (lt)
-        strftime(timestr, sizeof(timestr), "%H:%M:%S", lt);
-    else
-        snprintf(timestr, sizeof(timestr), "??:??:??");
+    /* Get time with millisecond precision */
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    time_t sec = tv.tv_sec;
+    struct tm lt_buf;
+    struct tm *lt = localtime_r(&sec, &lt_buf);
+    char timestr[16];
+    if (lt) {
+        char tmp[12];
+        strftime(tmp, sizeof(tmp), "%H:%M:%S", lt);
+        int ms = (int)(tv.tv_usec / 1000);
+        snprintf(timestr, sizeof(timestr), "%s.%03d", tmp, ms);
+    } else {
+        snprintf(timestr, sizeof(timestr), "??:??:??.???");
+    }
 
-    fprintf(stderr, "(%s) [%s] %s: ", timestr, level_str, tag);
+    /* Colorize output when stderr is a TTY */
+    int use_color = isatty(fileno(stderr));
+    const char *col_start = "";
+    const char *col_end = "";
+    if (use_color) {
+        switch (level) {
+            case LOG_ERROR:   col_start = "\x1b[31m"; break; /* red */
+            case LOG_WARN:    col_start = "\x1b[33m"; break; /* yellow */
+            case LOG_INFO:    col_start = "\x1b[32m"; break; /* green */
+            case LOG_DEBUG:   col_start = "\x1b[36m"; break; /* cyan */
+            case LOG_VERBOSE: col_start = "\x1b[35m"; break; /* magenta */
+            default:          col_start = ""; break;
+        }
+        col_end = "\x1b[0m";
+    }
+
+    fprintf(stderr, "%s(%s) [%s] %s: ", col_start, timestr, level_str, tag);
 
     va_list args;
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
     va_end(args);
 
-    fprintf(stderr, "\n");
+    fprintf(stderr, "%s\n", col_end);
 }
 
 #endif // LOG_H
