87e07032727ba7ce476d897ed990731d47a2ceba
marobeck <maysgaming00@gmail.com>
2026-03-11 14:23:16 -0700

    Ensure database exists on startup

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+
+from .database import init_db
 from .models import SyncRequest, SyncResponse
 from .sync import process_sync
 
 app = FastAPI()
+
+init_db()
 
 @app.post("/sync", response_model=SyncResponse)
 def sync(request: SyncRequest):
